a3b78baa6bf4736d17c17ebe24a3dd46f7c1990a
munib dalati <munib.dalati@gmail.com>
2023-04-26 14:32:44 +0300

    JS day1

---
+++ index.js
@@ -0,0 +1,79 @@
+let cash = 1000
+let current_liabilites = 500
+cash_flow_ratio = cash/current_liabilites
+console.log(cash_flow_ratio);
+
+let revenues = 1000
+let expenses = 500
+net_income = revenues-expenses
+console.log(net_income);
+
+let liabilities = 1000
+let equity = 500
+total_assets = liabilities+equity
+console.log(total_assets);
+
+let profit = 1000
+let sales = 500
+net_income = profit*sales
+console.log(net_income);
+
+let numbers =[7,9,2]
+average = (numbers[0]+numbers[1]+numbers[2])/3
+console.log(average);
+
+let price = 150
+let discount = 0.3
+Discount = price - (price*discount)
+console.log(Discount);
+
+let age=20
+console.log(age>=18 && age<=30);
+
+let number1 = 2
+let number2 = 3
+exponential = 2**3
+console.log(exponential);
+
+let num1 = 10
+let num2 = 4
+remainder = num1%num2
+console.log(remainder);
+
+console.log(typeof(100));
+console.log(typeof(73.9));
+console.log(typeof(NaN));
+console.log(typeof("water"));
+console.log(typeof(false));
+console.log(typeof(9 != 11));
+console.log("Orang" + "e");
+console.log("Orange" - "s");
+console.log("4" + "8");
+console.log("name" + 3);
+console.log("name" - 3);
+console.log(82 * "word");
+console.log(1 + "hello");
+console.log("hello" + 1);
+console.log(1 + true);
+console.log("hello" + true);
+console.log(typeof(Infinity));
+console.log(1 == '1');
+console.log(1 === '1');
+
+text = "Welcome to Orange"
+console.log(text.toUpperCase())
+to = text.charAt(8)+text.charAt(9)
+console.log(to.toUpperCase())
+console.log(text.replace("to", "from"));
+console.log(text.toLowerCase());
+console.log(text.length);
+console.log(text.replace("Orange", '"Orange"'));
+console.log(text.concat(" Jordan"));
+
+let example = "cacccctus"
+console.log("c" + example.slice(1).replace(/c/g, "*"));
+
+
+
+
+
